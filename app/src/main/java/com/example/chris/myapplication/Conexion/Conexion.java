e283ebd070ba0b28cd6ff5496a08836ebaf27dfa
Christian Poma Machicado <chris_yeah90@hotmail.com>
2016-05-29 20:40:33 -0500

    Commit

--- app/src/main/java/com/example/chris/myapplication/Conexion/Conexion.java
+++ app/src/main/java/com/example/chris/myapplication/Conexion/Conexion.java
@@ -1,11 +1,10 @@
 package com.example.chris.myapplication.Conexion;
 
-/**
- * Created by Chris on 29/05/2016.
- */
+
 public class Conexion {
     public static final String DATA_URL = "http://192.168.160.220:8080/Feed/feed.php?page=";
     public static final String TAG_IMAGE_URL = "image";
     public static final String TAG_NAME = "name";
     public static final String TAG_PUBLISHER = "publisher";
+    public static final String TAG_TIPO = "tipo";
 }
